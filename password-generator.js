f77fcceaa14a77edd4ee75d3f25ac1426933197e
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2019-12-15 14:58:35 -0500

    First major content push - WORK IN PROGRESS - Tearing down from here and building again from scratch.

--- password-generator.js
+++ password-generator.js
@@ -1,3 +1,83 @@
+//This is where JavaScript is able to select elements in my HTML by using document.getElementByID//
+
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const lowercaseEl = document.getElementById('lowercaseCheckbox');
+const uppercaseEl = document.getElementById('uppercaseCheckbox');
+const numberEl = document.getElementById('numberCheckbox');
+const specialEl = document.getElementById('specialCheckbox');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+//This is
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    special: getRandomSpecial
+};
+
+generateEl.addEventListener('click', () => {
+    const length = lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numberEl.checked;
+    const hasSpecial = specialEl.checked;
+
+    console.log(length, hasLower, hasUpper, hasNumber, hasSpecial);
+
+    resultEl.innerText = generatePassword(
+        hasLower,
+        hasUpper,
+        hasNumber,
+        hasSpecial,
+        length
+    );
+
+});
+
+function copy() {
+    let textarea = document.getElementById("result");
+    textarea.select();
+    document.execCommand("copy");
+  }
+
+function generatePassword(lower, upper, number, special, length) {
+    let generatedPassword = "";
+
+    const typesCount = lower + upper + number + special;
+    console.log("typesCount: " + typesCount)
+
+    const typesArray = [{lower}, {upper}, {number}, {special}].filter(item => Object.values(item)[0]);
+    console.log("typesArray: " + typesArray);
+
+    if (typesCount === 0) {
+        alert("You must select at least one character type.")
+        return '';
+    }
+
+    for (let i = 0; i < length; i+= typesCount) {
+        typesArray.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            console.log('functname:' + funcName)
+            generatedPassword += randomFunc[funcName]();
+            
+        });
+    }
+    let finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword;
+
+    
+}
+
+
+  
+
+
+
+
 
 
 //These are my functions for generating random characters.//
@@ -21,4 +101,3 @@ function getRandomSpecial() {
     return String.fromCharCode(Math.floor(Math.random() * 14) + 33)
 }
 console.log(getRandomSpecial())
-
