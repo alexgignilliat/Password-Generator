4f0ad369aea5d01934a96bb3225efcd283176b89
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2019-12-18 16:56:06 -0500

    Major JS overhaul after tutoring session. Less complex, less bloated.

--- password-generator.js
+++ password-generator.js
@@ -1,50 +1,50 @@
+//This is where JavaScript is able to select elements in my HTML by using document.getElementByID//
+//Grab html element for the result textarea
+var resultEl = document.getElementById('result');
+//Grab html element for the length form/field
+var lengthEl = document.getElementById('length');
+//Grab html element for the lowercase checkbox
+var lowercaseEl = document.getElementById('lowercaseCheckbox');
+//Grab html element for the uppercase checkbox
+var uppercaseEl = document.getElementById('uppercaseCheckbox');
+//Grab html element for the number checkbox
+var numberEl = document.getElementById('numberCheckbox');
+//Grab html element for the special checkbox
+var specialEl = document.getElementById('specialCheckbox');
+//Grab html element for the generate button
+var generateEl = document.getElementById('generate');
+//Grab html element for the copy to clipboard feature
+var clipboardEl = document.getElementById('clipboard');
+
+// var randomize = {
+//     lower: getRandomLower,
+//     upper: getRandomUpper,
+//     number: getRandomNumber,
+//     special: getRandomSpecial
+// };
+/*Every character is assigned a number. Example: 97 = lowercase "a".
+Using fromCharCode, any ASCII character can be retrieved.
+Pretty cool alternative to typing them all out into arrays.*/
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-console.log(getRandomLower())
-
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-console.log(getRandomUpper())
-
 function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 9) + 48)
+    return String.fromCharCode(Math.floor(Math.random() * 9) + 48);
 }
-console.log(getRandomNumber())
-
 function getRandomSpecial() {
-    return String.fromCharCode(Math.floor(Math.random() * 14) + 33)
+    return String.fromCharCode(Math.floor(Math.random() * 14) + 33);
 }
-console.log(getRandomSpecial())
-
-//This is where JavaScript is able to select elements in my HTML by using document.getElementByID//
-
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
-const lowercaseEl = document.getElementById('lowercaseCheckbox');
-const uppercaseEl = document.getElementById('uppercaseCheckbox');
-const numberEl = document.getElementById('numberCheckbox');
-const specialEl = document.getElementById('specialCheckbox');
-const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
-
-const randomize = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    special: getRandomSpecial
-};
-
-generateEl.addEventListener('click', () => {
-    const length = lengthEl.value;
-    const hasLower = lowercaseEl.checked;
-    const hasUpper = uppercaseEl.checked;
-    const hasNumber = numberEl.checked;
-    const hasSpecial = specialEl.checked;
-
-    console.log(length, hasLower, hasUpper, hasNumber, hasSpecial);
-
+//Event listener for the generate button.
+generateEl.addEventListener('click', function() {
+    var length = +lengthEl.value;
+    var hasLower = lowercaseEl.checked;
+    var hasUpper = uppercaseEl.checked;
+    var hasNumber = numberEl.checked;
+    var hasSpecial = specialEl.checked;
+// console.log(length, hasLower, hasUpper, hasNumber, hasSpecial);
     resultEl.innerText = generatePassword(
         hasLower,
         hasUpper,
@@ -52,45 +52,66 @@ generateEl.addEventListener('click', () => {
         hasSpecial,
         length
     );
-
 });
 
+function generatePassword(lower, upper, number, special, length){
+    var checkArr = []
+    if (lower === true){
+        checkArr.push(getRandomLower()) //try with and without ()
+    }
+    if (upper === true){
+        checkArr.push(getRandomUpper())
+    }
+    if (number === true){
+        checkArr.push(getRandomNumber())
+    }
+    if (special === true){
+        checkArr.push(getRandomSpecial())
+     } else "alert user"
+
+    //  console.log(checkArr)
+
+     var generatedPass = "";
+
+     for(i = 1; i < length; i++){
+        generatedPass += checkArr[Math.floor(Math.random() * checkArr.length)] //random number between 0-3 in an index
+     }
+     return generatedPass
+}
+
+// function generatePassword(lower, upper, number, special, length) {
+//     var generatedPass = "";
+//     var checkboxes = lower + upper + number + special;
+// // console.log("checkboxes: " + checkboxes)
+//     var checkArray = [{ lower }, { upper }, { number }, { special }].filter(item => Object.values(item)[0]);
+// // console.log("typesArray: " + typesArray);
+// //Passord length check
+//     if (length < 8 || length > 128) {
+//         alert("You must select a length between 8 - 128.")
+//         return '';
+//     }
+// //At least one checkbox must be selected to generate password
+//     if (checkboxes === 0) {
+//         alert("You must select at least one character type.")
+//         return '';
+//     }
+//     // for (var i = 0; i < length; i+= checkboxes) {
+//     //     checkArray.forEach(type => {
+//     //         var funcName = Object.keys(type)[0];
+//     //         console.log('functname:' + funcName)
+//     //         generatedPass += randomize[funcName]();
+            
+//     //     });
+//     //     }
+//     var finalPassword = generatedPass.slice(0, length);
+//     return finalPassword;
+// }
+
+//Function for copying to clipboard
 function copy() {
-    let textarea = document.getElementById("result");
+    var textarea = document.getElementById("result");
     textarea.select();
     document.execCommand("copy"); 
 }
 
-function generatePassword(lower, upper, number, special, length) {
-    let generatedPassword = "";
-
-    const typesCount = lower + upper + number + special;
-    console.log("typesCount: " + typesCount)
-
-    const typesArray = [{ lower }, { upper }, { number }, { special }].filter(item => Object.values(item)[0]);
-    console.log("typesArray: " + typesArray);
-
-    if (length < 8 || length > 128) {
-        alert("You must select a length between 8 - 128.")
-        return '';
-    }
-    
-    if (typesCount === 0) {
-        alert("You must select at least one character type.")
-        return '';
-    }
-
-    for (let i = 0; i < length; i+= typesCount) {
-        typesArray.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            console.log('functname:' + funcName)
-            generatedPassword += randomize[funcName]();
-            
-        });
-        }
-    let finalPassword = generatedPassword.slice(0, length)
-
-    return finalPassword;
-
-}
 
